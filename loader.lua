04b503b7a8c05902fd5ce8c826190dac763517eb
CodeXHubZ <119740825+CodeXHubZ@users.noreply.github.com>
2022-12-03 20:26:00 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,11 @@
+local gamelist = {
+    [3183403065]  = "https://raw.githubusercontent.com/CodeXHubZ/CodeXHubZ/main/ADA.lua",
+}
 
+local checkgame = gamelist[game.gameId]
+if gamelist[game.gameId] then 
+print (checkgame)
+    loadstring(game:HttpGet(checkgame))()
+else 
+game.Players.LocalPlayer:Kick("Not Game Support")
+end
