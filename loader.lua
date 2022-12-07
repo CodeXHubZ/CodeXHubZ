2145f2ad841d6f07300697095c92181d70c90dd8
CodeXHubZ <119740825+CodeXHubZ@users.noreply.github.com>
2022-12-07 22:20:30 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name)
+wait(5)
 local gamelist = {
     [3183403065]  = "https://raw.githubusercontent.com/CodeXHubZ/CodeXHubZ/main/ADA.lua",
 }
