424a82a933408d3bbd1ad9164b73b616ee654367
CodeXHubZ <119740825+CodeXHubZ@users.noreply.github.com>
2022-12-17 02:29:39 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,31 @@
-task.wait(15)
 repeat task.wait() until game:IsLoaded()
 local gamelist = {
     [3183403065]  = "https://raw.githubusercontent.com/CodeXHubZ/CodeXHubZ/main/ADA.lua",
     [2655311011]  = "https://raw.githubusercontent.com/CodeXHubZ/CodeXHubZ/main/Anime%20Dimensions%20Simulator.lua",
 }
 
+if game.gameId == 3183403065 then
+spawn(function()    
+if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Loading") then
+    _G.MapName = tostring(game:GetService("Players").LocalPlayer.PlayerGui.Loading.Info.Info.Location.Text)
+    _G.MapMode = tostring(game:GetService("Players").LocalPlayer.PlayerGui.Loading.Info.Info.Description.Text)
+    _G.MapDiff = tostring(game:GetService("Players").LocalPlayer.PlayerGui.Loading.Info.Info.Difficulty.Text)
+    end 
+end)
+repeat task.wait() until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Loading")    
 local checkgame = gamelist[game.gameId]
 if gamelist[game.gameId] then 
 print (checkgame)
     loadstring(game:HttpGet(checkgame))()
 else 
-game.Players.LocalPlayer:Kick("Not Game Support")
+    game.Players.LocalPlayer:Kick("Not Game Support")
+end
+    else
+    local checkgame = gamelist[game.gameId]
+    if gamelist[game.gameId] then 
+    print (checkgame)
+    loadstring(game:HttpGet(checkgame))()
+    else 
+    game.Players.LocalPlayer:Kick("Not Game Support")
+    end    
 end
