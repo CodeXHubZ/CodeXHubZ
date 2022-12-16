b9d6f6db55e3a289f6c93ccea00bb19c5bb193b9
CodeXHubZ <119740825+CodeXHubZ@users.noreply.github.com>
2022-12-17 02:25:30 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,24 +1,14 @@
+task.wait(15)
 repeat task.wait() until game:IsLoaded()
 local gamelist = {
     [3183403065]  = "https://raw.githubusercontent.com/CodeXHubZ/CodeXHubZ/main/ADA.lua",
     [2655311011]  = "https://raw.githubusercontent.com/CodeXHubZ/CodeXHubZ/main/Anime%20Dimensions%20Simulator.lua",
 }
 
-if game.gameId == 3183403065 then
-repeat task.wait() until not game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("Loading")    
 local checkgame = gamelist[game.gameId]
 if gamelist[game.gameId] then 
 print (checkgame)
     loadstring(game:HttpGet(checkgame))()
 else 
-    game.Players.LocalPlayer:Kick("Not Game Support")
-end
-    else
-    local checkgame = gamelist[game.gameId]
-    if gamelist[game.gameId] then 
-    print (checkgame)
-    loadstring(game:HttpGet(checkgame))()
-    else 
-    game.Players.LocalPlayer:Kick("Not Game Support")
-    end    
+game.Players.LocalPlayer:Kick("Not Game Support")
 end
